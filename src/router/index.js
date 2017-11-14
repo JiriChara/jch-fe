59fd665ef27e0358390a4820c9e320c5018c9c92
Jiri Chara <jiri@chara.vip>
2017-11-14 21:01:05 +0100

    Get rid of boilerpate

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import Articles from '@/pages/Articles';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      name: 'articles',
+      component: Articles,
     },
   ],
 });
