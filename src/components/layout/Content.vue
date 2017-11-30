f247bb4957be6ad9f04acc132ccb5ae5e0d3618c
Jiri Chara <jiri@chara.vip>
2017-11-30 13:56:45 +0100

    Clean unused file

--- src/components/layout/Content.vue
+++ src/components/layout/Content.vue
@@ -6,10 +6,6 @@
 
     <div class="column">
       <slot name="side">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-        no sea takimata sanctus est Lorem ipsum dolor sit amet.
       </slot>
     </div>
   </div>
